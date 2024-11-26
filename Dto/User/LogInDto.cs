bcd52af9e327faec9f9b8f504481fb75a24b97fd
Sahil Parsaniya <sahil.parsaniya270@gmail.com>
2024-11-27 00:13:55 +0530

    Done changes for Login Changes and updated exception handler

---
+++ Dto/User/LogInDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Dto.User
+{
+    public class LogInDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Service/UserService/UserService.cs
+++ Service/UserService/UserService.cs
@@ -1,8 +1,10 @@
 ﻿using Core;
+using Data.Models;
 using Dto;
 using Mapper;
 using Microsoft.EntityFrameworkCore;
 using Repo;
+using System.Linq.Expressions;
 
 namespace Service
 {
@@ -13,6 +15,7 @@ namespace Service
         private readonly ICategoryRepository _categoryRepository;
         private readonly IStateRepository _stateRepository;
         private readonly ICountryRepository _countryRepository;
+        private readonly UserDto _loggedInUser;
 
         private readonly UserMapper _userMapper;
         public UserService(
@@ -21,7 +24,8 @@ namespace Service
                 ICategoryRepository categoryRepository,
                 IStateRepository stateRepository,
                 ICountryRepository countryRepository,
-                UserMapper userMapper)
+                UserMapper userMapper,
+                UserDto loggedInUser)
         {
             _userRepo = userRepository;
             _roleRepository = roleRepository;
@@ -29,6 +33,7 @@ namespace Service
             _stateRepository = stateRepository;
             _countryRepository = countryRepository;
             _userMapper = userMapper;
+            _loggedInUser = loggedInUser;
         }
 
         public async Task AddUserAsync(UserDto userDto)
@@ -74,7 +79,7 @@ namespace Service
                             categoryDict[user.CategoryId])
                     )
                     .ToList();
-                
+
         }
 
         public async Task<bool> IsUserExistAsync(UserDto userDto)
@@ -115,65 +120,77 @@ namespace Service
             if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
                 throw new ApiException(401, $"Invalid {nameof(userName)} or {nameof(password)}");
 
+            var userData = await GetUsersQuery(x => EF.Functions.Like(x.Username, userName)
+                                                    || EF.Functions.Like(x.Email, userName)
+                                                    || EF.Functions.Like(x.PhoneNo, userName)).FirstOrDefaultAsync();
+
+            if (userData == null
+               || userData.Item1 == null
+               || string.IsNullOrEmpty(userData.Item1.Password)
+               || !userData.Item1.Password.Equals(password))
+            {
+                throw new ApiException(401, "Invalid Username or Password");
+            }
+
+            return _userMapper.GetUserDto(userData.Item1, userData.Item2, userData.Item3, userData.Item4, userData.Item5, true);
+        }
+
+        private IQueryable<Tuple<User, Country?, State?, Role?, Category?>> GetUsersQuery(Expression<Func<User, bool>> filter)
+        {
+            var usersQuery = _userRepo.Get(filter, false);
             var countryQuery = _countryRepository.GetQueyable(false);
             var stateQuery = _stateRepository.GetQueyable(false);
             var roleQuery = _roleRepository.GetQueyable(false);
             var categoryQuery = _categoryRepository.GetQueyable(false);
 
 
-            var user = (await  _userRepo.Get(x => SQLFunctions.Like(x.Username, userName))
-                                        .GroupJoin(
-                                            countryQuery, 
-                                            x => x.CountryId, 
-                                            y => y.Id, 
+            return usersQuery.GroupJoin(
+                                            countryQuery,
+                                            x => x.CountryId,
+                                            y => y.Id,
                                             (User, countries) => new { User, countries }
                                         )
-                                        .SelectMany(
-                                            x => x.countries.DefaultIfEmpty(),
-                                            (result, Country) => new { result.User, Country }
-                                        )
-                                        .GroupJoin(
-                                            stateQuery, 
-                                            x => x.User.StateId, 
-                                            y => y.Id, 
-                                            (result, states) => new { result.User, result.Country, states }
-                                        )
-                                        .SelectMany(
-                                            x => x.states.DefaultIfEmpty(), 
-                                            (result, State) => new { result.User, result.Country, State }
-                                        )
-                                        .GroupJoin(
-                                            roleQuery, 
-                                            x => x.User.RoleId, 
-                                            y => y.Id, 
-                                            (result, roles) => new { result.User, result.Country, result.State, roles }
-                                        )
-                                        .SelectMany(
-                                            x => x.roles.DefaultIfEmpty(), 
-                                            (result, Role) => new { result.User, result.Country, result.State, Role }
-                                        )
-                                        .GroupJoin(
-                                            categoryQuery, 
-                                            x => x.User.CategoryId, 
-                                            y => y.Id, 
-                                            (result, categories) => new { result.User, result.Country, result.State, result.Role, categories }
-                                        )
-                                        .SelectMany(
-                                            x => x.categories.DefaultIfEmpty(), 
-                                            (result, Category) => new { result.User, result.Country, result.State, result.Role, Category }
-                                        )
-                                        .FirstOrDefaultAsync());
+                             .SelectMany(
+                                 x => x.countries.DefaultIfEmpty(),
+                                 (result, Country) => new { result.User, Country }
+                             )
+                             .GroupJoin(
+                                 stateQuery,
+                                 x => x.User.StateId,
+                                 y => y.Id,
+                                 (result, states) => new { result.User, result.Country, states }
+                             )
+                             .SelectMany(
+                                 x => x.states.DefaultIfEmpty(),
+                                 (result, State) => new { result.User, result.Country, State }
+                             )
+                             .GroupJoin(
+                                 roleQuery,
+                                 x => x.User.RoleId,
+                                 y => y.Id,
+                                 (result, roles) => new { result.User, result.Country, result.State, roles }
+                             )
+                             .SelectMany(
+                                 x => x.roles.DefaultIfEmpty(),
+                                 (result, Role) => new { result.User, result.Country, result.State, Role }
+                             )
+                             .GroupJoin(
+                                 categoryQuery,
+                                 x => x.User.CategoryId,
+                                 y => y.Id,
+                                 (result, categories) => new { result.User, result.Country, result.State, result.Role, categories }
+                             )
+                             .SelectMany(
+                                 x => x.categories.DefaultIfEmpty(),
+                                 (result, Category) => new Tuple<User, Country?, State?, Role?, Category?>(
+                                     result.User,
+                                     result.Country,
+                                     result.State,
+                                     result.Role,
+                                     Category
+                                 )
+                             );
 
-            if (user == null
-               || user.User == null
-               || string.IsNullOrEmpty(user.User.Password)
-               || !user.User.Password.Equals(password))
-            {
-                throw new ApiException(401, "Invalid Username or Password");
-            }
-
-
-            return _userMapper.GetUserDto(user.User, user.Country, user.State, user.Role, user.Category, true);
         }
 
     }
